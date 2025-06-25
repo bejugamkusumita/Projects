4d4a9a2e3508e2a2809280caf73d82429e4d644c
bejugamkusumita <160478012+bejugamkusumita@users.noreply.github.com>
2025-06-25 21:44:10 +0530

    Update p.py

--- ProductCatalog/p.py
+++ ProductCatalog/p.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -41,5 +42,9 @@ def add_to_cart():
         print("Error:", e)
         return jsonify({"message": "Error saving cart items", "error": str(e)}), 500
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
