a2ca808fb6926036f2b06ecc3e366a624e631953
bejugamkusumita <160478012+bejugamkusumita@users.noreply.github.com>
2025-06-25 21:57:52 +0530

    Update and rename p.py to app.py

--- ProductCatalog/app.py
+++ ProductCatalog/app.py
@@ -41,10 +41,11 @@ def add_to_cart():
     except Exception as e:
         print("Error:", e)
         return jsonify({"message": "Error saving cart items", "error": str(e)}), 500
-
-
-
+@app.route('/')
+def home():
+    return "✅ Flask backend is live. POST to /add_to_cart to save cart data."
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
 
